16896bc894f02df41a72a3dfd06243ad8217a485
sabudilovskiy <sabudilovskiy@yandex.ru>
2021-07-06 23:43:01 +0300

    Переписана логика операторов и функций.

--- src/Archieve.java
+++ src/Archieve.java
@@ -2,20 +2,34 @@ import java.util.ArrayList;
 
 public class Archieve {
     ArrayList<Operator> base = new ArrayList<>();
-    ArrayList<Function> functions = new ArrayList<>();
     public Archieve(){
         int n = Id_lexemes.NUMBER_OPERATORS.get();
         for (int i = 0; i < n; i++)
         {
             base.add(new Operator());
-            functions.add(new Sin());
         }
+        add_operator( new Abs() );
+        add_operator(new Sin());
+        add_operator(new Cos());
+        add_operator(new Tg());
+        add_operator(new Ctg());
+        add_operator(new Arcsin());
+        add_operator( new Arccos());
+        add_operator( new Arctg() );
+        add_operator( new Arcctg() );
+        add_operator( new Exp() );
+        add_operator( new Ln() );
+        add_operator( new Log() );
+        add_operator( new Pow() );
+        add_operator( new Mult() );
+        add_operator( new Div() );
+        add_operator( new Plus() );
+        add_operator( new Mult() );
     }
-    public void add_operator(Operator A, Function function)
+    public void add_operator(Operator A)
     {
         int n = A.get_id();
         base.set(n, A);
-        this.functions.set(n, function);
     }
     ArrayList<Integer> decode(String input, ArrayList <Integer> verif)
     {
@@ -69,10 +83,10 @@ public class Archieve {
     }
     boolean check_countable(Id_lexemes id, ArrayList <Double> argues)
     {
-        return functions.get(id.get()).check(argues);
+        return base.get(id.get()).check(argues);
     }
     double count(Id_lexemes id, ArrayList <Double> argues)
     {
-        return functions.get(id.get()).count(argues);
+        return base.get(id.get()).count(argues);
     }
 }
--- src/Operator.java
+++ src/Operator.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
 public class Operator {
-    private Id_lexemes id;
-    private int left_argue = 0;
-    private int right_argue = 0;
-    private int priority;
-    private ArrayList<String> decode_base = new ArrayList<>();
+    protected Id_lexemes id;
+    protected Function func = new F_Sin();
+    protected int left_argue = 0;
+    protected int right_argue = 0;
+    protected int priority;
+    protected ArrayList<String> decode_base = new ArrayList<>();
     public int get_id(){
         return id.get();
     }
@@ -32,12 +33,19 @@ public class Operator {
         }
         return max_code;
     }
-    int get_priority()
+    public int get_priority()
     {
         return priority;
     }
+
+    public double count(ArrayList<Double> args){
+        return func.count(args);
+    }
+    public boolean check(ArrayList<Double> args){
+        return func.check(args);
+    }
     //id, количество аргументов слева, количество аргументов справа, приоритет, количество кодовых слов, кодовые слова
-    Operator(Id_lexemes id, int left_argue, int right_argue, int priority, ArrayList<String> list_of_words)
+    Operator(Function func, Id_lexemes id, int left_argue, int right_argue, int priority, ArrayList<String> list_of_words)
     {
         this.id = id;
         this.left_argue = left_argue;
@@ -51,5 +59,166 @@ public class Operator {
     Operator()
     {
         priority = 0;
+    }
+}
+class Abs extends Operator
+{
+    Abs(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Abs();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "abs" );
+    }
+
+}
+class Sin extends Operator{
+    Sin(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Sin();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "sin" );
+    }
+}
+class Cos extends Operator{
+    Cos(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Cos();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "cos" );
+    }
+}
+class Tg extends Operator{
+    Tg(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Tg();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "tg" );
+        decode_base.add("tan");
+    }
+}
+class Ctg extends Operator{
+    Ctg(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Ctg();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "ctg" );
+        decode_base.add("cotan");
+        decode_base.add( "cot" );
+    }
+}
+class Arcsin extends Operator{
+    Arcsin(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Arcsin();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "arcsin" );
+    }
+}
+class Arccos extends Operator{
+    Arccos(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Arccos();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "arccos" );
+    }
+}
+class Arctg extends Operator{
+    Arctg(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Arctg();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "arctg" );
+        decode_base.add("arctan");
+    }
+}
+class Arcctg extends Operator{
+    Arcctg(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Arcctg();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "arcctg" );
+        decode_base.add("arccotan");
+        decode_base.add( "arccot" );
+    }
+}
+class Exp extends Operator{
+    Exp(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Exp();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "exp" );
+    }
+}
+class Ln extends Operator{
+    Ln(){
+        left_argue = 0;
+        right_argue = 1;
+        func = new F_Ln();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "ln" );
+    }
+}
+class Log extends Operator{
+    Log(){
+        left_argue = 0;
+        right_argue = 2;
+        func = new F_Log();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_FUNC );
+        decode_base.add( "log" );
+    }
+}
+class Pow extends Operator{
+    Pow(){
+        left_argue = 1;
+        right_argue = 1;
+        func = new F_Pow();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_POW );
+        decode_base.add( "^" );
+    }
+}
+class Mult extends Operator{
+    Mult(){
+        left_argue = 1;
+        right_argue = 1;
+        func = new F_Mult();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_MULT_DIV );
+        decode_base.add( "*" );
+    }
+}
+class Div extends Operator{
+    Div(){
+        left_argue = 1;
+        right_argue = 1;
+        func = new F_Div();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_MULT_DIV );
+        decode_base.add( "/" );
+    }
+}
+class Minus extends Operator{
+    Minus(){
+        left_argue = 1;
+        right_argue = 1;
+        func = new F_Minus();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_PLUS_MINUS );
+        decode_base.add( "-" );
+    }
+}
+class Plus extends Operator{
+    Plus(){
+        left_argue = 1;
+        right_argue = 1;
+        func = new F_Plus();
+        priority = Priority_operators.getId( Priority_operators.PRIOR_PLUS_MINUS );
+        decode_base.add( "+" );
     }
 }
